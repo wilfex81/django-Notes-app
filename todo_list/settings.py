0094402467aad7d20b7b1c2db2a579b49a8b756d
wilfex <wilfex81@gmail.com>
2022-10-29 13:12:49 +0300

    trusted origins

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@=fd1qp&n*!1zxtsmy-l$e&=1p^6(pqxzp_=$lc=%9jafui15t
 DEBUG = True
 
 ALLOWED_HOSTS = ['django-notes-app-production.up.railway.app']
-
+CSRF_TRUSTED_ORIGINS = ['https://django-notes-app-production.up.railway.app/']
 
 # Application definition
 
