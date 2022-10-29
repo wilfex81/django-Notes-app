a2dfda040041953c72e8b59c755095406e3a2aaa
wilfex <wilfex81@gmail.com>
2022-10-29 13:35:03 +0300

    csrf

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,16 +25,16 @@ SECRET_KEY = 'django-insecure-@=fd1qp&n*!1zxtsmy-l$e&=1p^6(pqxzp_=$lc=%9jafui15t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-notes-app-production.up.railway.app']
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://django-notes-app-production.up.railway.app'
-# ],
-# CORS_ORIGIN_WHITELIST = [
-#     'https://django-notes-app-production.up.railway.app',
-# ]
 CORS_ALLOWED_ORIGINS = [
 'https://django-notes-app-production.up.railway.app',
 "http://localhost:8000"
+]
+ALLOWED_HOSTS = ['django-notes-app-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-notes-app-production.up.railway.app'
+],
+CORS_ORIGIN_WHITELIST = [
+    'https://django-notes-app-production.up.railway.app',
 ]
 # Application definition
 
